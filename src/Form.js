312eb40b4489efc30b46a3d6c37ad68e798cc747
Johann Folta <jfolta@kyruus.com>
2019-10-29 11:56:26 -0400

    form part 2 complete

--- src/Form.js
+++ src/Form.js
@@ -5,17 +5,25 @@ class Form1 extends React.Component {
     super();
     this.state = {
       firstName: "",
-      lastName: ""
+      lastName: "",
+      lifeStory: "Please write your lifestory here :)",
+      freeTicket: true,
+      gender: "",
+      favPet: "Bearded Dragon"
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   // with destructuring to prevent bugs
   handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({
+          [name]: checked
+        })
+      : this.setState({
+          [name]: value
+        });
   }
 
   // handleChange(event) {
@@ -38,7 +46,6 @@ class Form1 extends React.Component {
           ></input>
         </label>
         <br />
-
         <label>
           Last Name&nbsp;
           <input
@@ -48,6 +55,73 @@ class Form1 extends React.Component {
             onChange={this.handleChange}
           ></input>
         </label>
+        <br />
+        {/**
+         * Other useful form elements:
+         *
+         *  <textarea /> element
+         *  <input type="checkbox" />
+         *  <input type="radio" />
+         *  <select> and <option> elements
+         */}
+        <textarea
+          onChange={this.handleChange}
+          name="lifeStory"
+          value={this.state.lifeStory}
+        />
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="freeTicket"
+            checked={this.state.freeTicket}
+            onChange={this.handleChange}
+          />
+          Would you like a free ticket to Appleton Farms?
+        </label>
+        <br />
+
+        <label id="gender">
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          />
+          Male
+        </label>
+        <br />
+
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />
+          Female
+        </label>
+        <br />
+
+        <label id="petChoice">
+          Desired Pet &nbsp;
+          <select
+            name="favPet"
+            value={this.state.favPet}
+            onChange={this.handleChange}
+          >
+            <option value="Beared Dragon">Beared Dragon</option>
+            <option value="Guinea Pig">Guinea Pig</option>
+            <option value="Parrot">Parrot</option>
+            <option value="Tortoise">Tortoise</option>
+            <option value="Ferret">Ferret</option>
+            <option value="Goldfish">Goldfish</option>
+          </select>
+        </label>
+
         <h1>
           {this.state.firstName} {this.state.lastName}
         </h1>
